f82325f964d5961df95ba6d7ee52e60d1b236903
Maneet Khaira <maneetkhaira@Maneets-MacBook-Pro-2.local>
2025-03-09 17:29:29 -0700

    initial commit

---
+++ GameControllerTester/GameControllerTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameControllerTesterApp.swift
+//  GameControllerTester
+//
+//  Created by Maneet Khaira on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GameControllerTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GameControllerTesterTests/GameControllerTesterTests.swift
@@ -0,0 +1,16 @@
+//
+//  GameControllerTesterTests.swift
+//  GameControllerTesterTests
+//
+//  Created by Maneet Khaira on 3/9/25.
+//
+
+import Testing
+
+struct GameControllerTesterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
